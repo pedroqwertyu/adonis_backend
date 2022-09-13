875b0b2c2d41367eba0f93a1ff5beb8e70dcffa2
pedroqwertyu <pedro.h.m.f30@gmail.com>
2022-09-12 22:03:26 -0300

    seeders

--- database/seeders/Disciplina.ts
+++ database/seeders/Disciplina.ts
@@ -5,9 +5,9 @@ export default class extends BaseSeeder {
   public async run () {
     await Disciplina.createMany([
       {nome: 'Frontend', cursoId: 1}, 
-      {nome: 'Frontend', cursoId: 1}, 
-      {nome: 'Frontend', cursoId: 1}, 
-      {nome: 'Frontend', cursoId: 1}, 
+      {nome: 'backend', cursoId: 1}, 
+      {nome: 'banco de dados', cursoId: 1}, 
+      {nome: 'estrutura de dados', cursoId: 1}, 
     ])
   }
 }
