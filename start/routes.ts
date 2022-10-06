c869e63a28514092d7d271fdb8a264c7a2551464
pedroqwertyu <pedro.h.m.f30@gmail.com>
2022-10-05 21:13:43 -0300

    feitos as rotas com os controllers

--- start/routes.ts
+++ start/routes.ts
@@ -24,5 +24,22 @@ Route.get('/', async () => {
   return { eu: 'oi' }
 })
 
+Route.resource('/alunos', 'AlunosController').apiOnly()
+
+Route.resource('/aulas', 'AulasController').apiOnly()
+
+Route.resource('/chamadas', 'ChamadasController').apiOnly()
+
 Route.resource('/cursos', 'CursosController').apiOnly()
+
 Route.resource('/disciplinas', 'DisciplinasController').apiOnly()
+
+Route.resource('/professors', 'ProfessorsController').apiOnly()
+
+Route.resource('/salas', 'SalasController').apiOnly()
+
+Route.resource('/semestres', 'SemestresController').apiOnly()
+
+Route.resource('/turmaAlunos', 'TurmaAlunosController').apiOnly()
+
+Route.resource('/turmas', 'TurmasController').apiOnly()
